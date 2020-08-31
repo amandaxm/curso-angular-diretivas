6181ea72cafa65ba80cbefa125a5376ddfba3c63
Amanda Xavier Mariano <42248042+amandaxm@users.noreply.github.com>
2020-08-31 11:00:33 -0300

    NgIf

---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+  cursos: string[]=["Angular 2"];
+  mostrarCursos: boolean=false;
+
+  constructor() { }
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+  ngOnInit(): void {
+  }
+
+}
